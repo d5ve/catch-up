2caf6d21df8930fe04c47eea759227bde92bdff6
Dave Webb <github@d5ve.com>
2018-04-10 14:32:00 +1200

    WIP work to convert to sqlboiler

--- main.go
+++ main.go
@@ -1,50 +1,35 @@
 package main
 
+//go:generate sqlboiler mysql --wipe
+
 import (
+	"database/sql"
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/d5ve/catch-up/models"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/namsral/flag"
+	"gopkg.in/inconshreveable/log15.v2"
 	"time"
 )
 
-type CatchUp struct {
-	gorm.Model
-	Name      string
-	Details   string
-	StartDate time.Time
-	EndDate   time.Time
-	Options   []Option
-}
-
-type Option struct {
-	gorm.Model
-	CatchUpID int
-	Date      time.Time
-	Votes     []Vote
-}
-
-type Vote struct {
-	gorm.Model
-	YNM    string `gorm:"size:1"`
-	person string
-}
+var log = log15.New()
 
 func main() {
-	fmt.Println("At top of main()")
+	log.Info("At top of main()")
 
 	var port int
 	flag.IntVar(&port, "port", 3000, "webservice port")
 	flag.Parse()
-	fmt.Println("port:", port)
+	log.Info("Got port", "port", port)
 
-	db, err := gorm.Open("sqlite3", "test.db")
+	db, err := sql.Open("mysql", "catch_up:catch_up_password@tcp(0.0.0.0:3306)/catch_up?parseTime=true")
 	if err != nil {
-		panic("failed to connect database")
+		panic(err.Error())
 	}
-	defer db.Close()
+	log.Info("Got DB handle", "db", db)
 
-	db.AutoMigrate(&CatchUp{}, &Option{}, &Vote{})
+	catch_ups, err := models.CatchUps(db).All()
+	log.Info("Got CatchUps", "catch_ups", catch_ups)
 
 	// Mon Jan 2 15:04:05 -0700 MST 2006
 	d1 := "2018-03-10"
@@ -52,26 +37,30 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprint("failed to parse ", d1))
 	}
+	log.Info("Parsed startDate", "startDate", startDate)
 
 	d2 := "2018-03-17"
 	endDate, err := time.Parse("2006-01-02", d2)
 	if err != nil {
 		panic(fmt.Sprint("failed to parse ", d2))
 	}
+	log.Info("Parsed endDate", "endDate", endDate)
+	/*
+		options := getOptions(startDate, endDate)
 
-	options := getOptions(startDate, endDate)
+		c1 := CatchUp{
+			Name:      "First catchup",
+			Details:   "First attempt at a catchup record",
+			StartDate: startDate,
+			EndDate:   endDate,
+			Options:   options,
+		}
 
-	c1 := CatchUp{
-		Name:      "First catchup",
-		Details:   "First attempt at a catchup record",
-		StartDate: startDate,
-		EndDate:   endDate,
-		Options:   options,
-	}
-
-	db.Create(&c1)
+		db.Create(&c1)
+	*/
 }
 
+/*
 func getOptions(startDate time.Time, endDate time.Time) (options []Option) {
 	if startDate.After(endDate) {
 		startDate, endDate = endDate, startDate
@@ -82,3 +71,4 @@ func getOptions(startDate time.Time, endDate time.Time) (options []Option) {
 	}
 	return options
 }
+*/
