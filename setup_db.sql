cb0c7d82a26d5ffcbba26c346592719d1d023b88
Dave Webb <github@d5ve.com>
2018-04-10 14:59:25 +1200

    Add foreign key constraints. Better docker docs

--- setup_db.sql
+++ setup_db.sql
@@ -1,3 +1,7 @@
+START TRANSACTION;
+
+DROP DATABASE catch_up;
+
 CREATE DATABASE catch_up;
 
 USE catch_up;
@@ -13,14 +17,14 @@ CREATE TABLE `catch_up` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- TODO: foreign key constraints.
 CREATE TABLE `option` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `catchup_id` int(11) NOT NULL,
+    `catch_up_id` int(11) NOT NULL,
     `created_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
     `date` datetime NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`catch_up_id`) REFERENCES catch_up (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- TODO: foreign key constraints.
@@ -31,8 +35,11 @@ CREATE TABLE `vote` (
     `updated_at` datetime NOT NULL,
     `voter` varchar(255) NOT NULL,
     `ynm` char(1) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`option_id`) REFERENCES `option` (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE USER 'catch_up'@'%' IDENTIFIED BY 'catch_up_password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES ON catch_up.* TO 'catch_up'@'%';
+
+COMMIT;
